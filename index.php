c4dd2dc4888871b3703c5bb19cffba4f1ca56b17
luanzinhoddk <39702576+luanzinhoddk@users.noreply.github.com>
2018-08-21 21:11:37 -0300

    projetosenac01

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="resources/css/bootstrap.css">
+    <link rel="stylesheet" href="resources/">
+    <title>Banco-Pagína Inicial</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light"> <!-- Inicio da Barra de Navegação -->
+  <a class="navbar-brand" href="#">
+    <img src="resources/img/icone-banco.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Inicio
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Deposito<span class="sr-only">(Página atual)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Saque</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Emprestimo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ajuda</a>
+    <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+    </form>
+      </li>
+    </ul>
+  </div>
+</nav><!-- Fim da barra de nagevação -->
+<div class="container"> <!--Início do container-->
+    
+</div> <!--Fim do container -->
+</body>
+</html>
